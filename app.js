1aac825905615ba027a17b6effada117bd45d334
Moosakhan <144359037+moosakhan1@users.noreply.github.com>
2024-05-21 20:03:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,240 @@
+
+
+document.write("<h1>question no 1</h1>");
+var num1 = 3;
+var num2 = 5;
+var result = num1 + num2;
+document.write("Sum of " + num1 + " and " + num2 + " is " + result)
+
+var num3 = 6;
+var num4 = 4;
+var result = num3 - num4;
+console.log(result)
+
+var num5 = 4;
+var num6 = 4;
+var result = num5 * num6;
+console.log(result)
+
+var num7 = 7;
+var num8 = 8;
+var result = num7 / num8;
+console.log(result)
+
+var num8 = 9;
+var num9 = 10;
+var result = num8 % num9;
+console.log(result)
+
+document.write("<h1>question no 2</h1>");
+var num0;
+document.write("Value after variable declaration is undefined"+"<br>" )
+
+
+var num12 = 8;
+console.log("intial value " + num12)
+
+
+var num1 = 1;
+var newNum2 = ++num1;
+console.log("value after increment is " + newNum2);
+
+var num4 = 5;
+document.write("initial value is " + num4 + "<br>")
+
+var num14 = 5;
+var newNum3 = ++num14
+document.write("value after increment is " + newNum3 + "<br>")
+
+var num15 = 7;
+var num16 = 6;
+var result = num15 + num16;
+document.write("value after addition" + result + "<br>")
+
+var num17 = 13;
+var newnum4 = --num17;
+document.write("value after decrement" + newnum4 +"<br>")
+
+
+var num17 = 13;
+var newnum4 = --num17;
+console.log("value after decrement " +  newnum4 )
+
+var num18 = 3;
+var num19 = 3;
+var result = num18 / num19;
+document.write("the reminder is " + result)
+
+
+document.write("<h1>question no 3 </h1>")
+var num19 = 600;
+var num20 = 5;
+var result = num19 * num20;
+document.write("total cost to buy 5 ticket to a movie is " + result + "PKR" +"<br>")
+
+document.write("<h1> question no 4</h1>")
+var num1 = 0;
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1  + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1  + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1  + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1  + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+num1++
+document.write("4 * " + num1 + " = " + 4 * num1 + "<br />")
+
+document.write("<h1>question no 5 </h1>")
+var Celsius_temperature = 20;
+var formula = (Celsius_temperature * 9 / 5) + 32;
+document.write(Celsius_temperature + "  is  " + formula + "  f  " + "<br />" + "<br />")
+
+var Fahrenheit_temperature = 12;
+var formula1 = (Fahrenheit_temperature - 32) *5/9
+document.write(Fahrenheit_temperature + " is " + formula1 + "C" + "<br>" +"<br />"  )
+
+document.write("<h1> questin no 6 </h1>")
+var price1 = 650;
+var quantity = 3;
+var price2 = 100;
+var quantity1 = 7;
+var shoppingCharges =100;
+var totalCost = (price1*quantity) + (price2*quantity1) + (shoppingCharges);
+document.write("price of 1 item is " + price1 + "<br>")
+document.write("quantity of item 1 is " + quantity + "<br>")
+document.write("price of item 2 is " +price2 + "<br>")
+document.write("quantity of item 2 is " + quantity1 + "<br>")
+document.write("shopping charges " +shoppingCharges + "<br>")
+document.write("total of your order is " + totalCost)
+
+document.write("<h1>question no 7</h1>")
+var Totalmark = 980;
+var Markobtained = 804;
+var percentage = (Totalmark/Markobtained)*100;
+document.write("Total mark:" + Totalmark +"<br>");
+document.write("markobtained:" + Markobtained +"<br>");
+document.write("percentage" + percentage +"<br>");
+
+document.write("<h1>question no 8</h1>")
+var num19 = 10;
+var num20 = 25;
+var result=(num19*104.80)+(num20*28);
+document.write("Total currency in pkr " + result);
+
+document.write("<h1>question no 9</h1>");
+var num10 = 4;
+var answer = (num10 + 4) / 4*5
+document.write(answer)
+
+document.write("<h1>question no 10</h1>");
+var currentYear = 2024;
+var birthYear = 2003;
+var yourAge = currentYear%birthYear;
+document.write("current year : "+ currentYear + "<br>");
+document.write("birth year :"+ birthYear + "<br>")
+document.write("your age :"+ yourAge);
+
+document.write("<h1>question no 11</h1>");
+var radius = 20;
+var pi = 3.142;
+var circumference = 2*pi*radius;
+console.log(circumference);
+var area = pi*radius*2;
+document.write("radius of circle " + radius +"<br>");
+document.write("the circumference is :" + circumference +"<br>");
+document.write("the area is: " + area +"<br>");
+
+document.write("<h1>question no 12</h1>");
+var snack = "choclatechip";
+var currentAge = 21;
+var maximumAge = 100;
+var perDay = 1;
+var eat_total = maximumAge - currentAge;
+document.write("favourite snack :" + snack + "<br>");
+document.write("current age: "+ currentAge + "<br>");
+document.write( " Esrimated maximum_age "  +  maximumAge + "<br />")
+document.write( " amount of snacks per day "  + perDay  + "<br />")
+document.write( " “You will need 150  Chocolate chip to last you untid the ripe old age of "  + eat_total  + "<br />")
+ 
+
+document.write("<h1> chapter no 3 <br> question no 1 </h1>");
+
+alert("I am 21 year old ");
+
+
+alert("You have visited this site 14 times");
+
+
+var birthYear = 2005;
+document.write("my birthYear is:" +birthYear);
+
+document.write("<h1> question no 1 </h1>");
+var name1 = "John Doe"
+var productTitle = "XYZclothing";
+var quantity = 5;
+document.write(name1+ " ordered " + quantity + "T-shirt(s)on " + productTitle )
+
+alert("JohneDoe");
+alert("15 year old");
+alert("Hellow word");
+alert("my Email Address example@example.com");
+alert("I am trying to learn from the book A smarter way to learn JavaScript")
+document.write("<h1>question no 2</h1>")
+document.write("Yah I can write HTML content through javascript");
+alert("Welcome to JS land... \n Happy Coding!")
+alert("Javascript... \n Enter! a valid password")
+alert("welcome to js land")
+alert("Happy coading! prevent this page from creating additional dialog ")
+
+alert("Holle...i can run js through my web browser's console")
+
+
+alert("pizza" + "... \n" + "pizz" + "... \n" + "piz" + "... \n" + "pi" + "... \n" + "p" +"... \n")
+
+
+
+var res ="$moosa"
+var tes ="_khan"
+var eee ="only 2 variable are allowed e.g $hello & _hello"
+var ddd ="JS variable are not allowed"
+
+
+
+alert(eee)
+alert(ddd)
+
+alert(res)
+alert(tes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+ 
+
+
+
+
+
+
